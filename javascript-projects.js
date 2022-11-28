a67b959f03f33b414e549ea22cc98de0a46664d1
Alex Vasiliu <71417996+AlexV18@users.noreply.github.com>
2022-11-28 13:00:33 -0500

    Added project #4

--- javascript-projects.js
+++ javascript-projects.js
@@ -4,7 +4,8 @@ Return true if the given string is a palindrome. Otherwise, return false.
 
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
-Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into 
+the same case (lower or upper case) in order to check for palindromes.
 
 We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
 
@@ -73,7 +74,8 @@ All roman numerals answers should be provided in upper-case.
 
   /*
   Project #3: CAESARS CIPHER
-  One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+  One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher 
+  the meanings of the letters are shifted by some set amount.
 
 A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
 
@@ -101,3 +103,54 @@ All letters will be uppercase. Do not transform any non-alphabetic character (i.
   }
   
   rot13("SERR PBQR PNZC");
+
+/*
+  Project #4: TELEPHONE NUMBER VALIDATOR
+  Return true if the passed string looks like a valid US phone number.
+
+The user may fill out the form field any way they choose as long as it has the format of 
+a valid US number. The following are examples of valid formats for US numbers 
+(refer to the tests below for other variants):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+For this challenge you will be presented with a string such as 800-692-7753 or 8oo-six427676;laskdjf. Your job is to validate or reject the 
+US phone number based on any combination of the formats provided above. The area code is required. If the country code is provided, 
+you must confirm that the country code is 1. Return true if the string is a valid US 
+phone number; otherwise return false.
+  */
+
+
+// Definitely not the cleanest way to complete this project. 
+// It's my first time doing harder regex like this.
+  function telephoneCheck(str) {
+    var regex = /^-/;
+    if(regex.test(str)){ return false; }
+    regex = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/;
+    if(regex.test(str)){ return false; }
+    var regex = /^[^1]and[(]/g;
+    if(regex.test(str)){ return false; }
+    var regex = /^[0-9][0-9][0-9][-][0-9][0-9][0-9][-][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    regex = /^[1][" "][(][0-9][0-9][0-9][)][" "][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    regex = /^[(][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][)]/g;
+    if(regex.test(str)){ return false; }
+    regex = /^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]/;
+    if(regex.test(str)){ return true; }
+    regex = /^[(][0-9][0-9][0-9][)][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    regex = /^[1][" "][0-9][0-9][0-9][" "][0-9][0-9][0-9][" "][0-9][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    regex = /^[1][" "][0-9][0-9][0-9][-][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    regex = /^[1][(][0-9][0-9][0-9][)][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]/g;
+    if(regex.test(str)){ return true; }
+    return false;
+  }
+  
+  telephoneCheck("555-555-5555");
